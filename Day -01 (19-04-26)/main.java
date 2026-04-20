934c36a1c91de234b434f85e421f6a6b88a43138
mahi-555 <moniramahin555@gmail.com>
2026-04-20 09:58:00 +0600

    Update main.java

--- Day -01 (19-04-26)/main.java
+++ Day -01 (19-04-26)/main.java
@@ -3,19 +3,19 @@ public class main{
         Student s1=new Student();
         s1.name="mahin";
         s1.Roll=10;
-        s1.district="Bhola";
+        s1.district="jamalpur";
         s1.Number="01749272722";
         s1.Show();
         Student s2=new Student();
-        s2.name="Kutub Ali";
+        s2.name="mourin";
         s2.Roll=11;
-        s2.district="Cumilla";
+        s2.district="rongpur";
         s2.Number="01629277953";
         s2.Show();
         Student s3=new Student();
-        s3.name="Hukumat Ali";
+        s3.name="tiha";
         s3.Roll=12;
-        s3.district="chittagang";
+        s3.district="gazipur";
         s3.Number="01749123456";
         s3.Show();
     }
