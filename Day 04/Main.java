14d3b4b6a366f41cd0a6150f395b397b2067a3f3
mahi-555 <moniramahin555@gmail.com>
2026-04-22 07:59:36 +0600

    Add Main class with Dog instance and methods

---
+++ Day 04/Main.java
@@ -0,0 +1,10 @@
+// INHERITANCE
+
+Public class Main {
+  public static void main(String[] args) {
+    Dog myDog = new Dog();
+    myDog.name = "Buddy";
+    myDog.eat();
+    myDog.bark();
+  }
+}
