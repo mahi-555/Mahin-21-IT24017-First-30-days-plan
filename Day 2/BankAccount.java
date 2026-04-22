eec70f7a95931012b23de83a9c3eac5f6e61940d
mahi-555 <moniramahin555@gmail.com>
2026-04-22 07:17:19 +0600

    Add BankAccount class with deposit and withdraw methods

---
+++ Day 2/BankAccount.java
@@ -0,0 +1,38 @@
+
+class BankAccount {
+    private String accountHolder;
+    private double balance;
+    public BankAccount(String accountHolder, double initialBalance) {
+        this.accountHolder = accountHolder;
+        if (initialBalance >= 0) {
+            this.balance = initialBalance;
+        } else {
+            this.balance = 0;
+        }
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+    public double getBalance() {
+        return balance;
+    }
+    public void deposit(double amount) {
+        if (amount > 0) {
+            balance += amount;
+            System.out.println("Deposited: $" + amount);
+        } else {
+            System.out.println("Invalid deposit amount.");
+        }
+    }
+    public void withdraw(double amount) {
+        if (amount > 0 && amount <= balance) {
+            balance -= amount;
+            System.out.println("Withdrew: $" + amount);
+        } else {
+            System.out.println("Insufficient funds or invalid amount.");
+        }
+    }
+}
+
+        
