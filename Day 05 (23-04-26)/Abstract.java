1e9e9b63a2e08ce7d4ae7094639e5ace121e36e6
mahi-555 <moniramahin555@gmail.com>
2026-04-24 17:50:41 +0600

    Implement abstract class Shape and Circle subclass

---
+++ Day 05 (23-04-26)/Abstract.java
@@ -0,0 +1,18 @@
+abstract class Shape {
+  abstract void draw();
+  void display() {
+    System.out.println("This os a shape.");
+  }
+}
+class circle extends shape {
+  void draw() {
+    System.out.println("Drawing Circle");
+  }
+}
+public class Main{
+ public static void main(String [] args){
+   Shape shape = new Circle();
+   shape.draw();
+   shape.display();
+ }
+}
