9a27130aa295261b8d5b8af3f6ace9c9b1ac48a7
mahi-555 <moniramahin555@gmail.com>
2026-04-24 18:00:09 +0600

    Implement Book class and main method for display
    Added a Book class with a constructor and method to display book information. Implemented main class to create a Book instance and display its details.

---
+++ Day 05 (23-04-26)/InnerClass.java
@@ -0,0 +1,21 @@
+class Book {
+  private String title;
+  private String author;
+  public Book( String title, String author) {
+    this.title = title;
+    this.author = author;
+  }
+  public void displayBookInfo(){
+    System.ot.println("Library: "+ libraryName);
+    System.out.println("Book Title:"+ title);
+    System.out.println("Author:"+ author);
+  }
+}
+}
+public class main { 
+  public static void main(string[] args){
+    Library myLibrary = myLibrary.new Book ("1984","Georage Orwell");
+    myBook.displayBookInfo();
+  }
+}
+  
