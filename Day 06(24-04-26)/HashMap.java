b244162551e59f9ba1be7d6a4f5a332a91d0f716
mahi-555 <moniramahin555@gmail.com>
2026-04-25 15:31:52 +0600

    Add HashMap example with corrected syntax
    Fixes syntax errors and corrects variable names in HashMap example.

---
+++ Day 06(24-04-26)/HashMap.java
@@ -0,0 +1,14 @@
+import.java.util.HashMap;
+public class HahMapExample {
+  public static void main( String[] args) {
+    HashMap<String,Integer> ageMap = new HashMap<>();
+
+  age.Map.put("Alic", 30);
+    ageMap.put("Bob",25);
+    ageMap.put("Charlie",35);
+
+  System.out.println("Age Map:" + ageMap);
+    int alicAge = ageMap.get("Alice");
+    System.ot.println("Alice's Age : " + aliceAge);
+  }
+}
