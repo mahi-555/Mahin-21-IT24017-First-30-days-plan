043afefe13d5c1ff42e276a5b642d91060a6928e
mahi-555 <moniramahin555@gmail.com>
2026-04-26 07:28:01 +0600

    Create TreeSetExample with basic operations
    Fix syntax errors and improve TreeSet example.

---
+++ Day 07(25-04-26)/TreeSet.java
@@ -0,0 +1,18 @@
+import java.util.TreeSet;
+public class TreeSetExample {
+  public static void main (String[] args) {
+    TreeSet<Integer.Numbers = new TreeSet<>();
+
+    numbers.add(5);
+    numbers.add(3);
+    numbers.add(8);
+    numbers.add(1);
+    numbers.add(3);
+
+    system.out.println(" 5 is the present in the set.");
+  }
+  numbers.remove(8);
+system.out.println("After removal: " + numbers);
+}
+}
+    
