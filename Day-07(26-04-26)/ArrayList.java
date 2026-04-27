41f954932e81bce83ab4d904185a6499b035d37b
mahi-555 <moniramahin555@gmail.com>
2026-04-27 10:35:46 +0600

    Create Library class with book management methods
    Implement Library class with methods to add and display books.

---
+++ Day-07(26-04-26)/ArrayList.java
@@ -0,0 +1,11 @@
+import java.util.ArrayList;
+public class Library {
+private ArrayList<String> books;
+public Library () {
+books = new ArrayList<>();
+}
+public void addBook(String book ) {
+books.add(book);
+}
+public void displayBooks(){
+System.out.printlm
