a7a039e214ea957012dc6d153384b0d9a33d9191
mahi-555 <moniramahin555@gmail.com>
2026-05-03 19:15:34 +0600

    Update Readfile.java

--- Day-11(29-04-26)/Readfile.java
+++ Day-11(29-04-26)/Readfile.java
@@ -1,1 +1,26 @@
-
+import java.io.FileInputStream;
+import java.io.IOException;
+public class ReadBytes {
+  public static void main(String[] args) {
+    FileInputStream
+      fileInputStream = null;
+    try {
+      fileInputStream = new FileInputStream("example.txt");
+      int byteDate;
+      while (byteData = fileInputStream.read()!=-1) {
+        System.out.print((char) byteDate);
+      }
+    }
+    catch ( IOExpection e) {
+      e.printStackTrace();
+    } finally {
+      if (fileInputStream != null) {
+        try {
+          fileInputStream.c lose();
+        } catch (IOException e) { 
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
