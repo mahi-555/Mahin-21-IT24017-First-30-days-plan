c5df14b8590d14fd88d01f4aca375e2ed1a81e26
mahi-555 <moniramahin555@gmail.com>
2026-05-03 19:39:44 +0600

    Create Byte Stream.java

---
+++ Day-11(29-04-26)/Byte Stream.java
@@ -0,0 +1,9 @@
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.oi.IOException;
+public class FileCopy {
+  public static void main(String[] args) {
+    FileInputStream inputFile = null;
+    FileInputStream outputFile = null;
+    try {
+      
