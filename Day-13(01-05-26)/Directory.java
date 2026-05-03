0660b880407d6f8739658e3c2bdc77aba84e27e8
mahi-555 <moniramahin555@gmail.com>
2026-05-03 20:29:22 +0600

    Update Directory.java

--- Day-13(01-05-26)/Directory.java
+++ Day-13(01-05-26)/Directory.java
@@ -1,1 +1,17 @@
-
+import java.io.File;
+public class ListDirectoryContents {
+  public static void main(String[] args) {
+    String directoryPath = "Projects/Java";
+    File directory = new File(directoryPath);
+    String[] contents = directory.list();
+    if(contents !=Null) {
+      System.out.println(" Contents of " + directoryPath + ":");
+      for (String fileName : contents ) {
+        System.out.println(fileName);
+      }
+    } else {
+      System.out.println( "This directory is empty or does not exists.");
+    }
+  }
+}
+      
