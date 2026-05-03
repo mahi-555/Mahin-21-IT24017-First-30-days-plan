f2df1609b443c918eddc28ff47a3f3222924d04f
mahi-555 <moniramahin555@gmail.com>
2026-05-03 19:44:07 +0600

    Update Byte Stream.java

--- Day-11(29-04-26)/Byte Stream.java
+++ Day-11(29-04-26)/Byte Stream.java
@@ -6,4 +6,25 @@ public class FileCopy {
     FileInputStream inputFile = null;
     FileInputStream outputFile = null;
     try {
+      InputFile = new FileInputStream("source.txt");
+      outputFile = new FileOutputStream("destination.txt");
+      int byteData;
+      while (byteData = fileInputStream.read()!=-1) {
+        ouutputFille.write(byte);
+      }
+    }
+    catch ( IOExpection e) {
+      e.printStackTrace();
+    } finally {
+      if (fileInputStream != null) {
+        try {
+          fileInputStream.c lose();
+        } catch (IOException e) { 
+          e.printStackTrace();
+        }
+      }
+    }
+  }
+}
+      
       
