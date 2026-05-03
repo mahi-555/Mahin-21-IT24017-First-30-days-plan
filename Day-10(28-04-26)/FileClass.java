9a4863fa786a2ab2b2ad810df5cd957bfc70c43d
mahi-555 <moniramahin555@gmail.com>
2026-05-03 09:21:29 +0600

    Update FileClass.java

--- Day-10(28-04-26)/FileClass.java
+++ Day-10(28-04-26)/FileClass.java
@@ -1,1 +1,11 @@
-
+import java.io.File;
+public class FileExample {
+  publiv static void main(String[] args) {
+    File myFile = new File("example.txt");
+    if (myFile.exists()) {
+      System.out.println("File exists.");
+    } else {
+      System.out.println("File does not exist. ");
+    }
+  }
+}
