4bc5244b206ba79a16667fd74f1e3ac599fba538
mahi-555 <moniramahin555@gmail.com>
2026-05-03 20:39:46 +0600

    Update NIO.java

--- Day-13(01-05-26)/NIO.java
+++ Day-13(01-05-26)/NIO.java
@@ -1,1 +1,18 @@
 
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.io.IOException;
+public class CreateDirectonaryNIO {
+  public static void main(String[] args) {
+      Path path = Paths.get("Projects/NioExample");
+    
+    try {
+      Path createdDir = Files.createDirectories(path);
+       System.out.print("Directory created successfully: " + createdDir.toString());
+    }
+    catch ( IOExpection e) {
+      System.out.println("Failed to create directory: " + e.getMessage());
+      }
+    }
+  }
